dcd6edd4b818aa506669d46af1181d3243ecc2ca
softwareGalaxia70 <solucionesprog1234@gmail.com>
2020-04-16 12:41:55 -0500

    update/16/04

--- CRUDExampleVSCode/src/app/App.java
+++ CRUDExampleVSCode/src/app/App.java
@@ -1,29 +1,93 @@
 package app;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 import model.Person;
 import model.PersonDAO;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+
         PersonDAO model = new PersonDAO();
 
-        Person person = new Person();
+        int menu = Integer.parseInt(JOptionPane.showInputDialog("CRUD EXAMPLE\n"+
+                                                               "1- Insert Data.\n"+
+                                                               "2- Select Data.\n"+
+                                                               "3- Update Data.\n"+
+                                                               "4- Delete Data.\n"+
+                                                               "5- Exit."));
 
-        person.setIdNumber(1234);
-        person.setIdType("Cedula de Ciudadania");
-        person.setName("Juan Luis");
-        person.setLastName("Guerra");
-        person.setDateBirth("1994/05/09");
-        person.setEmail("Example@gmail.com");
-        person.setPhone("340567");
-        person.setGender("Masculino");
+        switch(menu){
 
-        if(model.addPerson(person)){
-            System.out.println("Guardado");
-        }else{
-            System.out.println("No Guardado");
+            case 1:
+                
+                Person person = new Person();
+
+                person.setIdNumber(1345);
+                person.setIdType("Cedula de Ciudadania");
+                person.setName("Andrea Carolina");
+                person.setLastName("Perez");
+                person.setDateBirth("1994/05/09");
+                person.setEmail("Example@gmail.com");
+                person.setPhone("340567");
+                person.setGender("Femenino");
+        
+                if(model.addPerson(person)){
+                    System.out.println("Guardado");
+                }else{
+                    System.out.println("No Guardado");
+                }
+
+            break;
+
+            case 2:
+
+                ArrayList<String> data=new ArrayList<>();
+                data.add("Cedula de Ciudadania");
+                data.add("1345");
+                ArrayList<Person> list = model.selectPerson("IDNumber", data);
+
+                for(int i=0; i<list.size(); i++){
+
+                    System.out.println(list.get(i).getIdNumber() + " " + list.get(i).getName() + " "+ list.get(i).getLastName());
+                
+                }
+                
+            break;
+
+            case 3:
+
+                person = new Person();
+
+                person.setId(2);
+                person.setIdNumber(1345);
+                person.setIdType("Cedula de Ciudadania");
+                person.setName("Maria Carolina");
+                person.setLastName("Perez");
+                person.setDateBirth("1994/05/09");
+                person.setEmail("ExampleT@gmail.com");
+                person.setPhone("876544");
+                person.setGender("Femenino");
+        
+                if(model.updatePerson(person)){
+                    System.out.println("Los datos han sido modificados de manera exitosa");
+                }else{
+                    System.out.println("No se pudo modificar la informacion");
+                }
+
+            break;
+
+            case 4:
+            break;
+
+            case 5:
+            break;
+
         }
+
+
 
     }
 }
--- CRUDExampleVSCode/src/model/PersonDAO.java
+++ CRUDExampleVSCode/src/model/PersonDAO.java
@@ -2,10 +2,13 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class PersonDAO {
 
     private PreparedStatement pst;
+    private ResultSet rs;
     
     public boolean addPerson (Person person){
 
@@ -54,6 +57,140 @@ public class PersonDAO {
 
 
         return state;
+    }
+
+
+    public ArrayList<Person> selectPerson(String filter, ArrayList<String> data){
+
+        ArrayList<Person> list = new ArrayList<>();
+        Person person;
+
+        Connection connect = null;
+
+        try{
+
+            connect = ConnectionPool.getInstance().getConnection();
+
+            if(connect != null){
+
+                String sql="";
+
+                switch (filter) {
+                    
+                    case "Name":  
+                        sql = "SELECT * FROM persona WHERE name REGEXP ? AND last_name REGEXP ?";
+                        pst = connect.prepareStatement(sql);
+                        pst.setString(1, data.get(0));
+                        pst.setString(2, data.get(0));                                             
+                    break;
+                
+                    case "IDNumber":                        
+                        sql = "SELECT * FROM persona WHERE id_type=? AND id_number=?";
+                        pst = connect.prepareStatement(sql);
+                        pst.setString(1, data.get(0));
+                        pst.setInt(2, Integer.parseInt(data.get(1)));                                             
+                    break;
+
+                    default:
+                        sql = "SELECT * FROM persona WHERE 1";
+                        pst = connect.prepareStatement(sql);                    
+                    break;
+
+                }
+
+                rs = pst.executeQuery();
+
+                while(rs.next()){
+
+                    person = new Person();
+                    
+                    person.setId(rs.getInt("id"));
+                    person.setIdType(rs.getString("id_type"));
+                    person.setIdNumber(rs.getInt("id_number"));
+                    person.setName(rs.getString("name"));
+                    person.setLastName(rs.getString("last_name"));
+                    person.setEmail(rs.getString("email"));
+                    person.setPhone(rs.getString("phone"));
+                    person.setDateBirth(rs.getString("date_of_birth"));
+                    person.setGender(rs.getString("gender"));
+
+                    list.add(person);
+
+                }
+
+
+            }else{
+                System.out.println("Conexión Fallida");
+            }
+
+
+        }catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        
+        }finally{
+            try {
+                ConnectionPool.getInstance().closeConnection(connect);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }        
+
+
+        return list;
+
+    }
+
+
+    public boolean updatePerson(Person person){
+
+        boolean state = false;
+        Connection connect = null;
+
+        try{
+
+            connect = ConnectionPool.getInstance().getConnection();
+
+            if(connect != null){
+
+                String sql = "UPDATE persona SET id_type=?, id_number=?, name=?, last_name=?, phone=?, email=?, date_of_birth=?, gender=? WHERE id=?";
+
+                pst = connect.prepareStatement(sql);
+                pst.setString(1, person.getIdType());
+                pst.setInt(2, person.getIdNumber());
+                pst.setString(3, person.getName());
+                pst.setString(4, person.getLastName());
+                pst.setString(5, person.getPhone());
+                pst.setString(6, person.getEmail());
+                pst.setString(7, person.getDateBirth());
+                pst.setString(8, person.getGender());
+                pst.setInt(9, person.getId());
+
+                int res = pst.executeUpdate();
+
+                state = res > 0;
+
+            }else{
+                System.out.println("Conexión Fallida");
+            }
+
+
+        }catch(Exception ex){
+
+            System.out.println(ex.getMessage());
+        
+        }finally{
+            try {
+                ConnectionPool.getInstance().closeConnection(connect);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+        return state;
+
+
     }
 
 
